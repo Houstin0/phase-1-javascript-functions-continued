1ec69316940e63a8cfcbc82c0f239a4148f040a7
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-06-14 08:00:20 +0300

    completed assinment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(event = "roller-skate") {
+    return `This Saturday, I want to ${event}!`
+  }
+  saturdayFun()
+  
+  function mondayWork(event = 'go to the office') {
+    return `This Monday, I will ${event}.`
+  }
+  mondayWork("This Monday, I will work from home.")
+  
+  function wrapAdjective(wrap = "*") {
+    return function(single = "special") {
+      return `You are ${wrap}${single}${wrap}!`
+    }
+  }
+
+  wrapAdjective("!")("a dedicated programmer")
